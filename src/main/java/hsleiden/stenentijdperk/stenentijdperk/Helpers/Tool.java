8021c41066c1b8d37e433ac25eff1f68194ba19b
Juan <juantombal@live.nl>
2020-06-05 14:37:43 +0200

    Classes toegevoegd , locaties op speelbord

--- src/main/java/hsleiden/stenentijdperk/stenentijdperk/Helpers/Tool.java
+++ src/main/java/hsleiden/stenentijdperk/stenentijdperk/Helpers/Tool.java
@@ -1,5 +1,17 @@
 package hsleiden.stenentijdperk.stenentijdperk.Helpers;
 
-public class Tool {
 
+public class Tool extends Locatiebord{
+    public Tool(int x, int y, int width, int height) {
+        super(x, y, width, height);
+    }
+
+    public void ontvangGereedschap(Speler speler){
+        // ontvangen gereedschap
+        Tableau tableau = speler.getTableau();
+        int gereedschap = tableau.getAantalTools();
+        if (gereedschap < 12) {
+            tableau.verhoogTool();
+        }
+    }
 }
